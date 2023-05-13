40f9fd67e22f386c4acd34451ab876f7dda8a142
Natanael Nascimento <121968785+nnascimentodigital@users.noreply.github.com>
2023-05-12 22:44:13 -0300

    Update _system.sh
    Instalando docker para o debian

--- lib/_system.sh
+++ lib/_system.sh
@@ -309,15 +309,20 @@ system_docker_install() {
   sleep 2
 
   sudo su - root <<EOF
-  apt install -y apt-transport-https \
-                 ca-certificates curl \
-                 software-properties-common
+  apt install -y ca-certificates curl gnupg
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  chmod a+r /etc/apt/keyrings/docker.gpg
   
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
-  apt install -y docker-ce
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+  apt-get update
+  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  
 EOF
 
   sleep 2
